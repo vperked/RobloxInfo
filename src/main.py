ec505229a3b0bf5eef735aaa924bb588c7b5c537
vperked <perked@riseup.net>
2024-07-16 17:43:24 -0400

    Got rid of userinfo, merged it into main.

--- src/main.py
+++ src/main.py
@@ -1,4 +1,5 @@
 from roblox import Client
+from roblox.utilities.exceptions import UserNotFound
 import json
 import asyncio
 
@@ -8,6 +9,8 @@ async def main():
    answer = input("Would you like to put your roblox token:")
    if answer.lower() == "y":
     await readToken()
+   elif answer.lower() == "n":
+      await noToken()
    else: 
       print("Invalid.")
 
@@ -15,9 +18,24 @@ async def readToken():
    with open("config.json", "r") as file:
       config_data = json.load(file) 
       robloToken = config_data["token"]
-   client.set_token(robloToken)
-   user = await client.get_authenticated_user()
-   print("Logged in as:", user.name)
+      client.set_token(robloToken)
+      user = await client.get_authenticated_user()
+      print("Logged in as:", user.name)
+      print("Followers count:",  await user.get_follower_count())
+      print ("Friend count:", await user.get_friend_count())
+      
+async def noToken():
+   while True:
+      data = input("Put a Username:")
+      try:
+         user = await client.get_user_by_username(data)
+         print("Followers count:",  await user.get_follower_count())
+         print ("Friend count:", await user.get_friend_count())
+
+      except UserNotFound:
+         print("Invalid Username!")
+      
+
    
    
 asyncio.run(main())
